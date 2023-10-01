71d8ae46d0ef3a948dd86eb0dd6781a1c0585a10
Amjad Iqbal <36040604+AmjadIqbal@users.noreply.github.com>
2023-10-01 23:01:32 +0500

    Update OperationPage.php
    Remove unused function

--- app/Http/Controllers/Admin/Operations/OperationPage.php
+++ app/Http/Controllers/Admin/Operations/OperationPage.php
@@ -5,12 +5,4 @@ namespace App\Http\Controllers\Admin\Operations;
 trait OperationPage
 {
 
-    public function includePage( $pageName )
-    {
-        $file = base_path("app/Http/Controllers/Admin/Pages/$pageName.php");
-        if (file_exists($file)) {
-            include $file;
-        }
-    }
-
 }
